364f2139b7b57dd08998329dbab21a01c974b55b
Rohan Tanwar <rohantanwar0809@gmail.com>
2024-04-05 12:34:10 +0530

    📦 NEW: nativewind, redux setup

---
+++ src/screens/Home.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import { UserState } from "../store/reducers/userSlice";
+
+const Home = () => {
+  const { name } = useSelector<RootState, UserState>((state) => state.user);
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold text-slate-400">Welcome {name}</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ src/screens/Listing.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Listing = () => {
+  return (
+    <View className="flex-1 bg-slate-400 items-center justify-center">
+      <Text>Listing</Text>
+    </View>
+  );
+};
+
+export default Listing;
