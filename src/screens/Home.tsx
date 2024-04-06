33bc11b7c95b5250c99f42995ed462616758b493
Rohan Tanwar <rohantanwar0809@gmail.com>
2024-04-07 02:41:03 +0530

    👌 IMPROVE: code cleanup

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,15 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Routes from '../routes/Routes';
-import { RootState } from '../app/store';
-import { ProductsState } from '../types';
+import { View, Text } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import Routes from "../routes/Routes";
+import { RootState } from "../store";
+import { ProductsState } from "../types";
 
 const Home = () => {
   // const { name } = useSelector<RootState, ProductsState>((state) => state.user);
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className='text-2xl font-bold text-slate-400'>Welcome User</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold text-slate-400">Welcome User</Text>
     </View>
   );
 };
